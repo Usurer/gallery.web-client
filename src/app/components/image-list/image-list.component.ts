2439f5c019bacfbce659feaa275de54371b202ac
Usurer <ivvlg@narod.ru>
2023-10-30 11:09:02 +0300

    WIP on image popup

--- src/app/components/image-list/image-list.component.ts
+++ src/app/components/image-list/image-list.component.ts
@@ -5,8 +5,9 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 import { ImageListStore } from '../../services/image-list.store';
-import { Observable, map, tap } from 'rxjs';
+import { Observable, filter, map, tap } from 'rxjs';
 import { ItemInfo } from '../../dto/item-info';
+import { Router, RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'glr-image-list',
@@ -24,8 +25,13 @@ export class ImageListComponent implements OnInit {
     );
 
   constructor(
-    private readonly imagesStore: ImageListStore
-  ) { }
+    private readonly imagesStore: ImageListStore,
+    private readonly router: Router
+  ) {
+    router.events.pipe(
+      filter(e => e instanceof RouterEvent)
+    );
+  }
 
   ngOnInit(): void {
     this.imagesStore.getImages(20);
