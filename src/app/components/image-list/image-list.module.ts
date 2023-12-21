ddee8b57098731ba61e626b7f3e82c6e73beb019
Usurer <ivvlg@narod.ru>
2023-12-21 22:55:00 +0300

    cleanup

--- src/app/components/image-list/image-list.module.ts
+++ src/app/components/image-list/image-list.module.ts
@@ -5,23 +5,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { ImageListRoutingModule } from './image-list.routing.module';
 import { ImagePopupModule } from '../image-popup/image-popup.module';
-import { ImageListDumbComponent } from './image-list-dumb.component';
 import { MatIconModule } from '@angular/material/icon';
-import {ScrollingModule} from '@angular/cdk/scrolling';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @NgModule({
-  declarations: [
-    ImageListComponent,
-    ImageListDumbComponent
-  ],
-  imports: [
-    CommonModule,
-    BrowserModule,
-    RouterModule,
-    ImagePopupModule,
-    ImageListRoutingModule,
-    MatIconModule, 
-    ScrollingModule
-  ]
+    declarations: [ImageListComponent],
+    imports: [
+        CommonModule,
+        BrowserModule,
+        RouterModule,
+        ImagePopupModule,
+        ImageListRoutingModule,
+        MatIconModule,
+        ScrollingModule,
+    ],
 })
 export class ImageListModule {}
