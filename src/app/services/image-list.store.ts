939f87644898b8686d7d24ab0ecf47133b64c766
Usurer <ivvlg@narod.ru>
2023-12-28 12:39:39 +0300

    Little refactoring to ListItems

--- src/app/services/image-list.store.ts
+++ src/app/services/image-list.store.ts
@@ -1,8 +1,8 @@
 import { ComponentStore, tapResponse } from '@ngrx/component-store';
-import { fromFetch } from 'rxjs/fetch';
 import { switchMap, catchError, Observable, EMPTY, withLatestFrom } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { ItemInfo } from '../dto/item-info';
+import { HttpClient } from '@angular/common/http';
 
 export interface ImagesState {
     images: ItemInfo[];
@@ -18,7 +18,7 @@ export interface ListItemsQuery {
     providedIn: 'root',
 })
 export class ImageListStore extends ComponentStore<ImagesState> {
-    constructor() {
+    constructor(private httpClient: HttpClient) {
         super({ images: [] });
     }
 
@@ -26,36 +26,26 @@ export class ImageListStore extends ComponentStore<ImagesState> {
         return query$.pipe(
             withLatestFrom(this.select((state) => state.images)),
             switchMap(([query, images]) => {
-                return fromFetch(
-                    `http://localhost:5279/Images/ListItems?parentId=${query.parentId}&take=${query.take ?? 10}&skip=${
-                        query.skip ?? images.length ?? 0
-                    }`,
-                    {
-                        cache: 'force-cache',
-                    }
-                ).pipe(
+                const take = query.take ?? 10;
+                const skip = query.skip ?? images.length ?? 0;
+                const extensions = ['.jpg', '.jpeg'].map((x) => `&extensions=${x}`).join('');
+                const url = `http://localhost:5279/Images/ListItems?parentId=${query.parentId}&take=${take}&skip=${skip}${extensions}`;
+
+                return this.httpClient.get<ItemInfo[]>(url).pipe(
                     tapResponse(
-                        // TODO: Can we do it in a better way pls?
-                        (imageList) =>
-                            imageList.json().then((x: ItemInfo[]) => {
-                                if (x && x.length > 0) {
-                                    this.addItems(
-                                        x.filter(
-                                            (k) =>
-                                                k.name.toLowerCase().endsWith('jpg') ||
-                                                k.name.toLowerCase().endsWith('jpeg')
-                                        )
-                                    );
-                                }
-                            }),
+                        (imageList) => {
+                            if (imageList?.length) {
+                                this.addItems(imageList);
+                            }
+                        },
                         (error) => {
-                            console.log(error);
+                            throw error;
                         }
                     )
                 );
             }),
-            catchError((err) => {
-                console.log(`Oops! An API access error! ${err}`);
+            catchError((error) => {
+                console.log(`Oops! An API access error! ${JSON.stringify(error)}`);
                 return EMPTY;
             })
         );
