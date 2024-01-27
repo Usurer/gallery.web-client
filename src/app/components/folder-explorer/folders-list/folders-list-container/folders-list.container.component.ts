480fa47d0d8938e85286e1cdd6a71cc9dc939dff
Usurer <ivvlg@narod.ru>
2024-01-27 23:17:02 +0300

    Animation to folders list show-hide

--- src/app/components/folder-explorer/folders-list/folders-list-container/folders-list.container.component.ts
+++ src/app/components/folder-explorer/folders-list/folders-list-container/folders-list.container.component.ts
@@ -2,12 +2,19 @@ import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core
 import { FoldersListStore } from '../folders-list.store';
 import { Observable } from 'rxjs';
 import { FolderInfo } from '../../../../dto/folder-info';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
     selector: 'glr-folders-list-container',
     templateUrl: './folders-list.container.component.html',
     styleUrls: ['./folders-list.container.component.scss'],
     providers: [FoldersListStore],
+    animations: [
+        trigger('myInsertRemoveTrigger', [
+            transition(':enter', [style({ opacity: 0 }), animate('300ms', style({ opacity: 1 }))]),
+            transition(':leave', [animate('100ms', style({ opacity: 0 }))]),
+        ]),
+    ],
 })
 export class FoldersListContainerComponent implements OnChanges {
     @Input()
